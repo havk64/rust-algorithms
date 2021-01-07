5a94db5cdbaf531c288e3edeaf7087811014a524
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-07 22:21:48 +0100

    add sort of vector of floats with sort_by and partial_cmp

--- sort_vectors/src/main.rs
+++ sort_vectors/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
+    // Basic sorting of a vector
     let mut vec = vec![1, 5, 10, 2, 15];
 
     vec.sort();
 
     assert_eq!(vec, vec![1, 2, 5, 10, 15]);
+
+    // Sort of floats via sort_by and PartialOrd::parcial_cmp
+    let mut vec = vec![1.1, 1.15, 5.5, 1.123, 2.0];
+
+    vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    assert_eq!(vec, vec![1.1, 1.123, 1.15, 2.0, 5.5]);
 }
