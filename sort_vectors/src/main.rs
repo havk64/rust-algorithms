ef9ec46426daba39147d8462d4b82016a4e2f5e4
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-08 22:36:33 +0100

    add struct sorting example

--- sort_vectors/src/main.rs
+++ sort_vectors/src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u32
+}
+
+impl Person {
+    pub fn new(name: String, age: u32) -> Self {
+        Person {
+            name,
+            age
+        }
+    }
+}
 fn main() {
     // Basic sorting of a vector
     let mut vec = vec![1, 5, 10, 2, 15];
@@ -12,4 +26,32 @@ fn main() {
     vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
     assert_eq!(vec, vec![1.1, 1.123, 1.15, 2.0, 5.5]);
+
+    // Sort struct
+    let mut people = vec![
+        Person::new("Zoe".to_string(), 25),
+        Person::new("Al".to_string(), 60),
+        Person::new("John".to_string(), 1)
+    ];
+    // Sort by derived natural order (Name, Age)
+    people.sort();
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("John".to_string(), 1),
+            Person::new("Zoe".to_string(), 25)
+        ]);
+
+    people.sort_by(|a,b| b.age.cmp(&a.age));
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("Zoe".to_string(), 25),
+            Person::new("John".to_string(), 1)
+        ]
+    );
 }
