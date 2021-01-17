ad4528c2d40b4d805b022cc00a07944723db3e47
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-17 16:27:35 +0100

    tests: add detailes tests on pop

--- linked-lists/lists/src/first.rs
+++ linked-lists/lists/src/first.rs
@@ -68,6 +68,36 @@ mod test {
                 }))
             },
             list
-        )
+        );
+
+        let first: Option<i32> = list.pop();
+
+        assert_eq!(first, Some(2));
+
+        assert_eq!(
+            List {
+                head: Link::More(Box::new(Node {
+                    elem: 6,
+                    next: Link::Empty,
+                }))
+            },
+            list
+        );
+
+        let second: Option<i32> = list.pop();
+
+        assert_eq!(second, Some(6));
+
+        assert_eq!(
+            List {
+                head: Link::Empty,
+            },
+            list
+        );
+
+        let last: Option<i32> = list.pop();
+
+        assert_eq!(last, None);
+
     }
 }
