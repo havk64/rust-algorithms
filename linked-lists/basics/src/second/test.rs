d1bafee0d7a0c0957bf7b04c3317182e15ab7fb1
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-22 23:33:16 +0100

    tests: add tests to implementation of Iter

--- linked-lists/basics/src/second/test.rs
+++ linked-lists/basics/src/second/test.rs
@@ -113,3 +113,14 @@ fn test_into_iter() {
     assert_eq!(iter.next(), Some(2));
     assert_eq!(iter.next(), Some(1));
 }
+
+#[test]
+fn iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+}
