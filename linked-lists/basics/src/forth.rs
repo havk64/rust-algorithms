fb5409d3529a13779861ca95491358c5c96b101e
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-26 22:31:58 +0100

    test: add unit tests for doubly-linked list

--- linked-lists/basics/src/forth.rs
+++ linked-lists/basics/src/forth.rs
@@ -68,3 +68,38 @@
         while self.pop_front().is_some() {}
     }
 }
+
+ #[cfg(test)]
+ mod test {
+     use super::List;
+ 
+     #[test]
+     fn basics() {
+         let mut list = List::new();
+ 
+         // Check empty list behaves right
+         assert_eq!(list.pop_front(), None);
+ 
+         // Populate list
+         list.push_front(1);
+         list.push_front(2);
+         list.push_front(3);
+ 
+         // Check normal removal
+         assert_eq!(list.pop_front(), Some(3));
+         assert_eq!(list.pop_front(), Some(2));
+ 
+         // Push some more just to make sure nothing's corrupted
+         list.push_front(4);
+         list.push_front(5);
+ 
+         // Check normal removal
+         assert_eq!(list.pop_front(), Some(5));
+         assert_eq!(list.pop_front(), Some(4));
+ 
+         // Check exhaustion
+         assert_eq!(list.pop_front(), Some(1));
+         assert_eq!(list.pop_front(), None);
+     }
+ }
+ 
