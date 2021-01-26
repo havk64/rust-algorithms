ba8959c264fe09ec748779edf9390e2d2796e0b1
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-26 22:30:23 +0100

    feat: implement (not so correct) doubly-linked list

---
+++ linked-lists/basics/src/forth.rs
@@ -0,0 +1,64 @@
+ use std::rc::Rc;
+ use std::cell::RefCell;
+
+ pub struct List<T> {
+     head: Link<T>,
+     tail: Link<T>,
+}
+
+ type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+ struct Node<T> {
+     elem: T,
+     next: Link<T>,
+     prev: Link<T>,
+ }
+
+ impl<T> Node<T> {
+     fn new(elem: T) -> Rc<RefCell<Self>> {
+         Rc::new(RefCell::new(Node {
+             elem: elem,
+             next: None,
+             prev: None,
+         }))
+     }
+ }
+
+ impl<T> List<T> {
+     pub fn new() -> Self {
+         List { head: None, tail: None }
+     }
+
+     pub fn push_front(&mut self, elem: T) {
+         let new_head = Node::new(elem);
+
+
+         match self.head.take() {
+             Some(old_head) => {
+                 old_head.borrow_mut().prev = Some(new_head.clone());
+                 new_head.borrow_mut().next = Some(old_head);
+                 self.head = Some(new_head);
+             }
+             None => {
+                 self.tail = Some(new_head.clone());
+                 self.head = Some(new_head);
+             }
+         }
+     }
+
+     pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    self.tail.take();
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+
+        })
+    }
+ }
