524b74fa9a2c091d79f285bfa269bf704369e8ea
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-27 23:14:45 +0100

    refactor: remove comments

--- linked-lists/basics/src/forth.rs
+++ linked-lists/basics/src/forth.rs
@@ -79,27 +79,21 @@
      fn basics() {
          let mut list: List<i32> = List::new();
  
-         // Check empty list behaves right
          assert_eq!(list.pop_front(), None);
  
-         // Populate list
          list.push_front(1);
          list.push_front(2);
          list.push_front(3);
  
-         // Check normal removal
          assert_eq!(list.pop_front(), Some(3));
          assert_eq!(list.pop_front(), Some(2));
  
-         // Push some more just to make sure nothing's corrupted
          list.push_front(4);
          list.push_front(5);
  
-         // Check normal removal
          assert_eq!(list.pop_front(), Some(5));
          assert_eq!(list.pop_front(), Some(4));
  
-         // Check exhaustion
          assert_eq!(list.pop_front(), Some(1));
          assert_eq!(list.pop_front(), None);
      }
